cf89cd0ad36b8670926f6e63ff8a4ab93d0e9fc4
Роман <averis.ukr@gmail.com>
2026-05-27 20:12:21 +0300

    Добавьте файлы проекта.

---
+++ Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreKeeper.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Username { get; set; } = string.Empty;
+
+        // null = пароль не потрібен
+        public string? PasswordHash { get; set; }
+
+        // Спеціальний прапор, який дає всі права (адмін)
+        public bool IsAdmin { get; set; }
+
+        // Окремі дозволи (тільки якщо IsAdmin == false)
+        public bool CanViewProducts { get; set; }
+        public bool CanEditProducts { get; set; }
+        public bool CanDeleteProducts { get; set; }
+
+        public bool CanViewDishes { get; set; }
+        public bool CanEditDishes { get; set; }
+        public bool CanDeleteDishes { get; set; }
+
+        public bool CanCreateInvoices { get; set; }      // Прихід/розхід
+        public bool CanPrintInvoices { get; set; }
+
+        public bool CanManageRoles { get; set; }         // Керування ролями/користувачами
+        public bool CanViewLogs { get; set; }
+
+        // Метод перевірки права
+        public bool HasPermission(Func<User, bool> permissionCheck)
+        {
+            if (IsAdmin) return true;
+            return permissionCheck(this);
+        }
+    }
+}
